bc31c57c4e235ae6371e03bda8019013c89c059c
Rohit Daryanani <rohitdaryanani@yahoo.com>
2015-07-31 16:27:06 +0800

    replace view engine with handlebars

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
+var bodyParser   = require('body-parser');
+var mongoose     = require('mongoose');
+var hbs          = require('hbs');
 
 var routes = require('./routes/index');
 var users  = require('./routes/users');
@@ -14,7 +15,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hjs');
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
