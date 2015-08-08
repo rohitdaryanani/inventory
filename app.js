b333ca565f3fd817927673a172fc7b6f4d14e04d
Rohit Daryanani <rohitdaryanani@yahoo.com>
2015-08-08 19:54:24 +0800

    partial views support

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine('html', hbs.__express);
+hbs.registerPartials(__dirname + '/views/partials');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
