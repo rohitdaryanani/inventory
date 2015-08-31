b84c74d78406828c03f13a7a39b9a03803632304
Rohit Daryanani <rohitdaryanani@yahoo.com>
2015-08-31 16:50:37 +0800

    seperate for users

--- routes/index.js
+++ routes/index.js
@@ -68,36 +68,4 @@ router.get('/logout', function (req, res, next) {
 	res.redirect('/');
 })
 
-router.post('/signup', function ( req, res, end ) {
-	var user = new Users ( {
-		username : req.body.username,
-		email    : req.body.email,
-		password : bcrypt.hashSync( req.body.password, salt)
-	} )
-
-	user.save( function ( err, user ) {
-    	if( err.message.indexOf('duplicate key error') > -1 ) {
-			req.flash( 'info', 'username or email already exist.' );
-			res.redirect('/');
-			console.error( err.message )
-    	}
-
-    	if ( err.message.indexOf('Users validation failed') > -1 ) {
-    		req.flash( 'info', 'Please fill in all fields.' );
-			res.redirect('/');
-			console.error( err.message )
-    	}
-
-    	if ( err ) {
-    		req.flash('ooops something went wrong, Please try again.')
-    		res.redirect('/');
-    		console.error(err);
-    	}
-
-		session          = req.session;
-		session.username = user.username
- 		res.redirect('/items');
-	} )
-} )
-
 module.exports = router;
